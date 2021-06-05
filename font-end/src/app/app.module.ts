845329059bd01018cbccbd420c16e868a5efc559
Cuong Ly <cuong.ly@contemi.com.vn>
2021-06-06 00:23:17 +0700

    Add loading

--- font-end/src/app/app.module.ts
+++ font-end/src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
-import { KeywordComponent } from './keyword/keyword.component';
-import { RegisterComponent } from './register/register.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthComponent} from './auth/auth.component';
+import {KeywordComponent} from './keyword/keyword.component';
+import {RegisterComponent} from './register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MainLayoutComponent} from "./shared/layouts/main-layout.component";
 import {CommonModule} from "@angular/common";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ToastrModule} from "ngx-toastr";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {TokenInterceptor} from "./auth/token.interceptor";
 import {KeywordDetailComponent} from "./keyword/details/keyword.detail.component";
 import {KeywordUploadComponent} from "./keyword/upload/keyword.upload.component";
 import {ProfileComponent} from "./auth/profile/profile.component";
+import {NgProgressModule} from "ngx-progressbar";
+import {NgProgressHttpModule} from "ngx-progressbar/http";
 
 @NgModule({
   declarations: [
@@ -27,7 +29,7 @@ import {ProfileComponent} from "./auth/profile/profile.component";
     MainLayoutComponent,
     KeywordDetailComponent,
     KeywordUploadComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +39,10 @@ import {ProfileComponent} from "./auth/profile/profile.component";
     HttpClientModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    NgProgressHttpModule,
+    NgProgressModule.withConfig({
+      color: "red"
+    }),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
