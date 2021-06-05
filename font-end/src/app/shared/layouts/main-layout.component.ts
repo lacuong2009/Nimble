31e508049032a53c8850248297a9f26eb4a8704f
Cuong Ly <cuong.ly@contemi.com.vn>
2021-06-05 21:42:13 +0700

    add logout

--- font-end/src/app/shared/layouts/main-layout.component.ts
+++ font-end/src/app/shared/layouts/main-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {AuthService} from "../../auth/auth.service";
 
 @Component({
   selector: 'x-layout',
@@ -6,14 +8,41 @@ import { Component, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
+
+  public active:string = '';
+
   constructor(
     public el: ElementRef,
+    private router: Router,
+    private authService: AuthService
   ) {
+    router.events.subscribe((event: any) => {
+      if(event instanceof NavigationEnd) {
+        let url = event.url;
+        this.setActiveItem(url);
+      }
+    });
   }
 
   ngOnInit() {
   }
 
   ngOnDestroy() {
+  }
+
+  public setActiveItem(url: string)
+  {
+    if (url.indexOf('keyword')) {
+      this.active = 'keyword';
+    } else if (url.indexOf('register')) {
+      this.active = 'register';
+    } else if (url.indexOf('profile')) {
+      this.active = 'profile';
+    }
+  }
+
+  public logout() {
+    this.authService.logout();
+    window.location.href = '/';
   }
 }
